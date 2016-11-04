4ad5e65d7451948ab725fe64b0b5665d8818698b
Noah Guy <noahriguy@gmail.com>
2016-11-04 16:05:58 -0500

    add more helper methods (list_names and check_birthdays)

--- reminderinator.rb
+++ reminderinator.rb
@@ -4,6 +4,8 @@ require 'date'
 db = SQLite3::Database.new("info.db")
 db.results_as_hash = true
 
+today = DateTime.now
+
 create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS birthdayInfo (
   	id INTEGER PRIMARY KEY,
@@ -20,9 +22,11 @@ db.execute(create_table_cmd)
 #Helper methods. Aim is to:
 #Add a new person to the birthday list when required
 #Update any aspect of a person's info in the database
-#
+#Delete any person to the list when desired
+#IF you view a person's info and their birthday is today, you will get a snarky notification
 def create_new_info(db, name, age, birthday, preferences= "N/A")
 	db.execute("INSERT INTO birthdayInfo(name, age, birthday, preferences) VALUES (?, ?, ?, ?)", [name, age, birthday, preferences])
+  db.execute("SELECT * FROM birthdayInfo")
 end
 
 def update_age(db, newAge, id)
@@ -49,5 +53,40 @@ def view_info(db, id)
   db.execute("SELECT * FROM birthdayInfo WHERE id = ?"[id])
 end
 
+def view_all(db)
+  db.execute("SELECT * FROM birthdayInfo")
+  end
+end
+
+def list_choices
+  puts "What would you like to do today? Please select from one of these options (Input the number of the option):"
+  puts "1. Look at Birthday List"
+  puts "2. Add to List"
+  puts "3. Delete from List"
+  puts "4. Check for birthdays"
+  puts "5. Update Values"
+  puts "6. Exit Program"
+end
+
+def display_names(db)
+ count = 1
+ info = db.execute("SELECT * FROM birthdayInfo")
+ info.each do |inf|
+    puts "#{count}. inf['name']"
+    count += 1
+ end
+end
+
+def check_birthdays(db)
+  info = db.execute("SELECT * FROM birthdayInfo")
+  info.each do |inf|
+    birthdayString = inf['birthday'].split("/")
+    if(today.month.to_i == birthdayString[0].to_i && today.day.to_i == birthdayString[1].to_i)
+      puts "#{inf['name']}\'s birthday is today! Did you remember?"
+    end
+  end
+end
+
 puts "Welcome to the Reminderinator, where we remember your friends' birthdays so you don't have to!"
-puts "What would you like to do today?"
+puts "What would you like to do today? Please select from one of these options (Input the number of the option):"
+list_choices
